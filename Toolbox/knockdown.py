c769eb99f785af0c25b52e1e7c859e46408b8db4
mkwapien <m.t.kwapien@student.tudelft.nl>
2024-06-20 17:07:12 +0200

    Add calcs for both finite and ininite width SCF

--- Toolbox/knockdown.py
+++ Toolbox/knockdown.py
@@ -16,9 +16,9 @@ def CAI_laminate(Eave,E11,E22,v12,G12,R,w):
   
     num = 1 + (l + (1-l*v12**2*(E22/E11))*np.sqrt(2*(np.sqrt(E11/E22) -v12)+(E11/G12))) + ((E11/G12)-v12)*np.sqrt(E22/E11)
     den = 1 + l*(l+(1+np.sqrt(E22/E11)*np.sqrt(2*(np.sqrt(E11/E22) -v12)+(E11/G12)))) +((E11/G12)-2*l*v12)*np.sqrt(E22/E11)-(1-l)**2*v12**2*(E22/E11)
-    k_inf = 1 - (1-l)*num/den
-    SCF = (2+(1-(2*R/w)**3))/ ( 3 * ( 1 - (2*R/w))) * k_inf 
-    return SCF
+    SCF1 = 1 - (1-l)*num/den
+    SCF2 = (2+(1-(2*R/w)**3))/ ( 3 * ( 1 - (2*R/w))) * SCF1
+    return max(SCF1,SCF2)
 
 def CAI_strength(strength,SCF):
     return strength/SCF 
