c7b7dfb2ab75e41417ebc8f19df9c57635cffffe
mkwapien <m.t.kwapien@student.tudelft.nl>
2024-06-20 16:24:04 +0200

    Finalize Kd calcs

--- Toolbox/knockdown.py
+++ Toolbox/knockdown.py
@@ -1,20 +1,24 @@
 import numpy as np
-import scipy as sp
 
 
 
-def CAI_laminate():
-
-    def scf_5_2(l, v12, E11, E22, G12,a):
-        '''
-        
-        '''
-        num = 1 + (l + (1-l*v12**2*(E22/E11))*np.sqrt(2*(np.sqrt(E11/E22) -v12)+(E11/G12))) + ((E11/G12)-v12)*np.sqrt(E22/E11)
-        den = 1 + l*(l+(1+np.sqrt(E22/E11)*np.sqrt(2*(np.sqrt(E11/E22) -v12)+(E11/G12)))) +((E11/G12)-2*l*v12)*np.sqrt(E22/E11)-(1-l)**2*v12**2*(E22/E11)
-        k_inf = 1 - (1-l)*num/den
-        SCF = (2+(1-(2*a/2)**3))/ ( 3 * ( 1 - (2*a/2))) * k_inf 
-        return SCF
+def CAI_laminate(Eave,E11,E22,v12,G12,R,w):
     
+    '''
+    Eave - average modulus of the damaged laminate 
+    E11 -
+    R - radius 
+    w - width
+    Make sure units are consistent
+    '''
+
+    l = Eave/E11
+  
+    num = 1 + (l + (1-l*v12**2*(E22/E11))*np.sqrt(2*(np.sqrt(E11/E22) -v12)+(E11/G12))) + ((E11/G12)-v12)*np.sqrt(E22/E11)
+    den = 1 + l*(l+(1+np.sqrt(E22/E11)*np.sqrt(2*(np.sqrt(E11/E22) -v12)+(E11/G12)))) +((E11/G12)-2*l*v12)*np.sqrt(E22/E11)-(1-l)**2*v12**2*(E22/E11)
+    k_inf = 1 - (1-l)*num/den
+    SCF = (2+(1-(2*R/w)**3))/ ( 3 * ( 1 - (2*R/w))) * k_inf 
+    return SCF
+
 def CAI_strength(strength,SCF):
-    s_CAI = strength/SCF
-    return s_CAI
+    return strength/SCF 
